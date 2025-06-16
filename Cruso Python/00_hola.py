7029f044bddb6b0c4e0e04b516d9902a0caae698
Alberto Belló <albertobellorico@gmail.com>
2025-06-16 14:01:05 +0200

    Add files via upload

---
+++ Cruso Python/00_hola.py
@@ -0,0 +1,22 @@
+mensaje="Hola mundo cruel"
+
+print(mensaje)
+
+# Esto es un comentario
+
+print("Hola Python")
+
+'''
+Esto es
+un comentario
+en varias lineas
+'''
+
+# Consultar el tipo de dato
+
+print(type("Hola Python")) # tipo de dato 'str'
+print(type(5)) # tipo de dato 'int'
+print(type(1.5)) # tipo de dato 'float'
+print(type(1 + 2j)) # tipo de dato 'complex number'
+print(type(True)) # tipo de dato 'bool'
+
---
+++ Cruso Python/01_variables.py
@@ -0,0 +1,26 @@
+# Variables
+
+MyVariable = "My String variable"
+print(MyVariable)
+
+my_varibale = "mi string"
+print(my_varibale)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = True
+print(my_bool_variable)
+
+# Se pueden agrupar las variables para que se ejecuten juntas
+
+print(MyVariable, my_varibale, my_int_variable, my_bool_variable)
+print("Este es el valor de:", my_bool_variable)
+
+# Funciones del sistema
+print(len(my_varibale))
+
+# Variables en una sola linea
+name, surname, alias, age = "Alberto", "Bello", "SrDelicias", 33
+print(name, surname, alias, age)
+print("Me llamo:", name, surname, ". Mi edad es:", age, ". Mi alias es:",alias)
